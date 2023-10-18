655b3ca7cd5ab26e5bc26d81975389f4d3494137
YactayoC <syactayocrisostomo@gmail.com>
2023-10-18 09:27:42 -0500

    add column user_verified in table users

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask
-from src.routes.auth import auth_bp
-from src.database.db_pg import db
 from dotenv import load_dotenv
 import os
 
+from src.routes.auth import auth_bp
+from src.database.db_pg import db
+from src.utils.send_mail import configure_mail
+
+# Import models
+from src.models.roles import Roles
+from src.models.contacts import Contacts
+from src.models.sessions import Sessions
+from src.models.messages import Messages
+from src.models.users import Users
+
 load_dotenv()
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app)
-app.register_blueprint(auth_bp)
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    app.register_blueprint(auth_bp)
+    mail = configure_mail(app)
+    return app
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,14 +1,17 @@
 # Database
+from flask import current_app
+
 from src.database.db_pg import db
-from src.models.user import User
+from src.models.users import Users
 from src.utils.security import Security
+from src.utils.send_mail import send_email, configure_mail
 
 
 class AuthService:
     @classmethod
     def login_user(cls, email, password):
         try:
-            user = User.query.filter_by(email=email).first()
+            user = Users.query.filter_by(email=email).first()
 
             # Si el usuario no existe o la contraseña es incorrecta, devuelve un error
             # if not user or not check_password_hash(user.password, password):
@@ -28,7 +31,7 @@ class AuthService:
             return {
                 "error": str(e),
                 "success": False,
-            }
+            }, 500
 
     @classmethod
     def register_user(cls, user_data):
@@ -40,20 +43,34 @@ class AuthService:
             date_of_birth = user_data["date_of_birth"]
             cellphone = user_data["cellphone"]
 
-            if User.query.filter_by(email=email).first():
+            if Users.query.filter_by(email=email).first():
                 return {"error": "El usuario ya existe."}, 400
 
-            new_user = User(
+            new_user = Users(
                 email=email,
                 password=password,
                 name=name,
                 lastname=lastname,
                 date_of_birth=date_of_birth,
                 cellphone=cellphone,
+                token_email="token_email",
+                token_phone="token_phone",
+                language="es",
+                user_verified=0,
+                role_id=2,
+                created_at="2021-01-01 00:00:00",
             )
-
             db.session.add(new_user)
             db.session.commit()
+
+            mail = configure_mail(current_app)
+            isSend = send_email(mail, email, name)
+
+            if not isSend:
+                return {
+                    "message": "Usuario registrado exitosamente, pero no se pudo enviar el correo de bienvenida.",
+                    "success": True,
+                }, 400
 
             return {
                 "message": "Usuario registrado exitosamente.",
@@ -61,7 +78,8 @@ class AuthService:
             }, 201
 
         except Exception as e:
+            print(e)
             return {
                 "error": str(e),
                 "success": False,
-            }
+            }, 500
